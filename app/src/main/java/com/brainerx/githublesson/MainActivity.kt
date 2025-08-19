1f3451a01ea83783de9a61af005e4012faae840a
dahoumeziane <hd_meziane@esi.dz>
2025-08-19 11:45:03 +0100

    splash screen implemented

--- app/src/main/java/com/brainerx/githublesson/MainActivity.kt
+++ app/src/main/java/com/brainerx/githublesson/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.brainerx.githublesson
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.brainerx.githublesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val thread = Thread {
+            try {
+                Thread.sleep(2000) // 2 seconds
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            } finally {
+                // Switch to MainActivity
+                runOnUiThread {
+                    val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+                    startActivity(intent)
+                    finish() // Close splash so it won’t come back
+                }
+            }
         }
+        thread.start()
+
     }
 }
