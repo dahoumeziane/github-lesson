490af0d80bf2a26efeb8ecdd99bba682ac700b56
dahoumeziane <hd_meziane@esi.dz>
2025-08-19 12:07:06 +0100

    login added

--- app/src/main/java/com/brainerx/githublesson/RegisterActivity.kt
+++ app/src/main/java/com/brainerx/githublesson/RegisterActivity.kt
@@ -1,20 +1,23 @@
 package com.brainerx.githublesson
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.brainerx.githublesson.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var binding : ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.goLogin.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity,LoginActivity::class.java))
         }
     }
 }
